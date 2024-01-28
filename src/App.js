a7b77b40d0d9cb368af891704e0ec040fcda504a
Julia <juuh2382@gmail.com>
2024-01-28 17:55:37 -0300

    projeto finalizado

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Sidebar from './Sidebar/Sidebar';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar/>
+      <Header></Header>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
